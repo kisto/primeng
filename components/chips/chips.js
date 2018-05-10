8f1bceca6b6bf8e6bb5219b1c96ed0c8ffffa1e5
jurajkisztner <juraj.kisto@gmail.com>
2018-05-10 22:02:38 +0200

    new 2

---
+++ components/chips/chips.js
@@ -0,0 +1,204 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var common_1 = require("@angular/common");
+var shared_1 = require("../common/shared");
+var inputtext_1 = require("../inputtext/inputtext");
+var domhandler_1 = require("../dom/domhandler");
+var forms_1 = require("@angular/forms");
+exports.CHIPS_VALUE_ACCESSOR = {
+    provide: forms_1.NG_VALUE_ACCESSOR,
+    useExisting: core_1.forwardRef(function () { return Chips; }),
+    multi: true
+};
+var Chips = (function () {
+    function Chips(el, domHandler) {
+        this.el = el;
+        this.domHandler = domHandler;
+        this.onAdd = new core_1.EventEmitter();
+        this.onRemove = new core_1.EventEmitter();
+        this.allowDuplicate = true;
+        this.onFocus = new core_1.EventEmitter();
+        this.onBlur = new core_1.EventEmitter();
+        this.onModelChange = function () { };
+        this.onModelTouched = function () { };
+    }
+    Chips.prototype.ngAfterContentInit = function () {
+        var _this = this;
+        this.templates.forEach(function (item) {
+            switch (item.getType()) {
+                case 'item':
+                    _this.itemTemplate = item.template;
+                    break;
+                default:
+                    _this.itemTemplate = item.template;
+                    break;
+            }
+        });
+    };
+    Chips.prototype.onClick = function (event) {
+        this.inputViewChild.nativeElement.focus();
+    };
+    Chips.prototype.writeValue = function (value) {
+        this.value = value;
+        this.updateMaxedOut();
+    };
+    Chips.prototype.registerOnChange = function (fn) {
+        this.onModelChange = fn;
+    };
+    Chips.prototype.registerOnTouched = function (fn) {
+        this.onModelTouched = fn;
+    };
+    Chips.prototype.setDisabledState = function (val) {
+        this.disabled = val;
+    };
+    Chips.prototype.resolveFieldData = function (data, field) {
+        if (data && field) {
+            if (field.indexOf('.') == -1) {
+                return data[field];
+            }
+            else {
+                var fields = field.split('.');
+                var value = data;
+                for (var i = 0, len = fields.length; i < len; ++i) {
+                    value = value[fields[i]];
+                }
+                return value;
+            }
+        }
+        else {
+            return null;
+        }
+    };
+    Chips.prototype.onInputFocus = function (event) {
+        this.focus = true;
+        this.onFocus.emit(event);
+    };
+    Chips.prototype.onInputBlur = function (event) {
+        this.focus = false;
+        if (this.addOnBlur && this.inputViewChild.nativeElement.value) {
+            this.addItem(event, this.inputViewChild.nativeElement.value);
+            this.inputViewChild.nativeElement.value = '';
+        }
+        this.onModelTouched();
+        this.onBlur.emit(event);
+    };
+    Chips.prototype.removeItem = function (event, index) {
+        if (this.disabled) {
+            return;
+        }
+        var removedItem = this.value[index];
+        this.value = this.value.filter(function (val, i) { return i != index; });
+        this.onModelChange(this.value);
+        this.onRemove.emit({
+            originalEvent: event,
+            value: removedItem
+        });
+        this.updateMaxedOut();
+    };
+    Chips.prototype.addItem = function (event, item) {
+        this.value = this.value || [];
+        if (item && item.trim().length) {
+            if (this.allowDuplicate || this.value.indexOf(item) === -1) {
+                this.value = this.value.concat([item]);
+                this.onModelChange(this.value);
+                this.onAdd.emit({
+                    originalEvent: event,
+                    value: item
+                });
+            }
+        }
+        this.updateMaxedOut();
+    };
+    Chips.prototype.onKeydown = function (event) {
+        switch (event.which) {
+            //backspace
+            case 8:
+                if (this.inputViewChild.nativeElement.value.length === 0 && this.value && this.value.length > 0) {
+                    this.value = this.value.slice();
+                    var removedItem = this.value.pop();
+                    this.onModelChange(this.value);
+                    this.onRemove.emit({
+                        originalEvent: event,
+                        value: removedItem
+                    });
+                }
+                break;
+            //enter
+            case 13:
+                this.addItem(event, this.inputViewChild.nativeElement.value);
+                this.inputViewChild.nativeElement.value = '';
+                event.preventDefault();
+                break;
+            case 9:
+                if (this.addOnTab && this.inputViewChild.nativeElement.value !== '') {
+                    this.addItem(event, this.inputViewChild.nativeElement.value);
+                    this.inputViewChild.nativeElement.value = '';
+                    event.preventDefault();
+                }
+                break;
+            default:
+                if (this.max && this.value && this.max === this.value.length) {
+                    event.preventDefault();
+                }
+                break;
+        }
+    };
+    Chips.prototype.updateMaxedOut = function () {
+        if (this.inputViewChild && this.inputViewChild.nativeElement) {
+            if (this.max && this.value && this.max === this.value.length)
+                this.inputViewChild.nativeElement.disabled = true;
+            else
+                this.inputViewChild.nativeElement.disabled = false;
+        }
+    };
+    Chips.decorators = [
+        { type: core_1.Component, args: [{
+                    selector: 'p-chips',
+                    template: "\n        <div [ngClass]=\"'ui-chips ui-widget'\" [ngStyle]=\"style\" [class]=\"styleClass\" (click)=\"onClick($event)\">\n            <ul [ngClass]=\"{'ui-inputtext ui-state-default ui-corner-all':true,'ui-state-focus':focus,'ui-state-disabled':disabled}\">\n                <li #token *ngFor=\"let item of value; let i = index;\" class=\"ui-chips-token ui-state-highlight ui-corner-all\">\n                    <span *ngIf=\"!disabled\" class=\"ui-chips-token-icon fa fa-fw fa-close\" (click)=\"removeItem($event,i)\"></span>\n                    <span *ngIf=\"!itemTemplate\" class=\"ui-chips-token-label\">{{field ? resolveFieldData(item,field) : item}}</span>\n                    <ng-container *ngTemplateOutlet=\"itemTemplate; context: {$implicit: item}\"></ng-container>\n                </li>\n                <li class=\"ui-chips-input-token\">\n                    <input #inputtext type=\"text\" [attr.id]=\"inputId\" [attr.placeholder]=\"(value && value.length ? null : placeholder)\" [attr.tabindex]=\"tabindex\" (keydown)=\"onKeydown($event)\" \n                        (focus)=\"onInputFocus($event)\" (blur)=\"onInputBlur($event)\" [disabled]=\"disabled\" [ngStyle]=\"inputStyle\" [class]=\"inputStyleClass\">\n                </li>\n            </ul>\n        </div>\n    ",
+                    providers: [domhandler_1.DomHandler, exports.CHIPS_VALUE_ACCESSOR]
+                },] },
+    ];
+    /** @nocollapse */
+    Chips.ctorParameters = function () { return [
+        { type: core_1.ElementRef, },
+        { type: domhandler_1.DomHandler, },
+    ]; };
+    Chips.propDecorators = {
+        "style": [{ type: core_1.Input },],
+        "styleClass": [{ type: core_1.Input },],
+        "disabled": [{ type: core_1.Input },],
+        "onAdd": [{ type: core_1.Output },],
+        "onRemove": [{ type: core_1.Output },],
+        "field": [{ type: core_1.Input },],
+        "placeholder": [{ type: core_1.Input },],
+        "max": [{ type: core_1.Input },],
+        "tabindex": [{ type: core_1.Input },],
+        "inputId": [{ type: core_1.Input },],
+        "allowDuplicate": [{ type: core_1.Input },],
+        "inputStyle": [{ type: core_1.Input },],
+        "inputStyleClass": [{ type: core_1.Input },],
+        "addOnTab": [{ type: core_1.Input },],
+        "addOnBlur": [{ type: core_1.Input },],
+        "onFocus": [{ type: core_1.Output },],
+        "onBlur": [{ type: core_1.Output },],
+        "inputViewChild": [{ type: core_1.ViewChild, args: ['inputtext',] },],
+        "templates": [{ type: core_1.ContentChildren, args: [shared_1.PrimeTemplate,] },],
+    };
+    return Chips;
+}());
+exports.Chips = Chips;
+var ChipsModule = (function () {
+    function ChipsModule() {
+    }
+    ChipsModule.decorators = [
+        { type: core_1.NgModule, args: [{
+                    imports: [common_1.CommonModule, inputtext_1.InputTextModule, shared_1.SharedModule],
+                    exports: [Chips, inputtext_1.InputTextModule, shared_1.SharedModule],
+                    declarations: [Chips]
+                },] },
+    ];
+    return ChipsModule;
+}());
+exports.ChipsModule = ChipsModule;
+//# sourceMappingURL=chips.js.map
---
+++ components/terminal/terminalservice.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var Subject_1 = require("rxjs/Subject");
+var TerminalService = (function () {
+    function TerminalService() {
+        this.commandSource = new Subject_1.Subject();
+        this.responseSource = new Subject_1.Subject();
+        this.commandHandler = this.commandSource.asObservable();
+        this.responseHandler = this.responseSource.asObservable();
+    }
+    TerminalService.prototype.sendCommand = function (command) {
+        if (command) {
+            this.commandSource.next(command);
+        }
+    };
+    TerminalService.prototype.sendResponse = function (response) {
+        if (response) {
+            this.responseSource.next(response);
+        }
+    };
+    TerminalService.decorators = [
+        { type: core_1.Injectable },
+    ];
+    return TerminalService;
+}());
+exports.TerminalService = TerminalService;
+//# sourceMappingURL=terminalservice.js.map
